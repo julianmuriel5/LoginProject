d173086118295c47ac6854b4c178a1c05d574386
Juliàn <july.rlz@gmail.com>
2021-09-16 15:18:04 -0300

    primera parte

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:ital@1&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Por favor conectarse o crear usuario</h1>
+
+    <a href="login.php">Ingresar</a> o 
+    <a href="crearuser.php">Crear usuario</a> 
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:ital@1&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Login</h1>
+     <form action="login.php" method="post">
+         <input type="text" name="user"  placeholder="ingresar usuario">
+         <input type="password" name="password" placeholder="ingresar contraseña">
+         <input type="submit" value="Send">
+
+
+     </form>
+    
+</body>
+</html>
