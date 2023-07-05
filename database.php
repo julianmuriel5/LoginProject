94ce9fa2ff8b399b446b0f38ce6333cfd55b3804
julianmuriel5 <july.rlz@gmail.com>
2023-07-05 11:57:15 -0300

    erro bd corregido

--- database.php
+++ database.php
@@ -3,11 +3,11 @@
  $server = 'localhost' ;
  $username = 'root';
  $password = '';
- $database = 'php_login_database';
+ $database = 'tecnocut';
 
 
     try {
-    $conn = new PDO ("mysql:host=$sever;dbname=$database;",$username, $password);
+    $conn = new PDO ("mysql:host=$server;$database=tecnocut;",$username, $password);
     } catch (PDOexception $e) {
       die('connect failed: '.$e->getMessage());
     }
